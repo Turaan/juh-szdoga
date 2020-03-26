0972ad4fcd92776855a49384ecb9b6caeabf8370
Turaan <48063665+Turaan@users.noreply.github.com>
2020-03-26 09:40:08 +0100

    Add files via upload

---
+++ Juhász doga/TuranBalazsfootgolf/TuranBalazsfootgolf/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TuranBalazsfootgolf
+{
+    class Program
+    {
+        static List<Versenyzok> resztvevok;
+        static void Main(string[] args)
+        {
+            Beolvas();
+            F03();
+            F04();
+
+
+            Console.ReadKey();
+        }
+        private static  void F04()
+        {
+
+
+
+
+            double nok = resztvevok.Count(a => a.Kategoria.ToLower().Contains("noi"));
+            double arany = (nok * 100) / resztvevok.Count();
+            Console.WriteLine("f04 : {0:0.00}%", arany);
+
+        }
+        private static void F03()
+        {
+            
+                Console.WriteLine($"f03:  {resztvevok.Count}");
+             
+            
+        }
+
+        private static void Beolvas()
+        {
+            resztvevok = new List<Versenyzok>();
+            var sr = new StreamReader(@"C:\Users\Turan\OneDrive\Asztali gép\Juhász doga\TuranBalazsfootgolf\TuranBalazsfootgolf\bin\Debug\fob2016.txt", Encoding.UTF8);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                resztvevok.Add(new Versenyzok(sr.ReadLine()));
+            }
+            sr.Close();
+
+        }
+    }
+}
---
+++ Juhász doga/TuranBalazsfootgolf/TuranBalazsfootgolf/Versenyzok.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuranBalazsfootgolf
+{
+    class Versenyzok
+    {
+        public string Nev { get; set; }
+        public string Kategoria { get; set; }
+
+        public string Egyesulet { get; set; }
+
+        public byte[] Pontok{ get; set; }
+
+        public Versenyzok(string s)
+        {
+            var t = s.Split(';');
+            Nev = t[0];
+            Kategoria = t[1];
+            Egyesulet = t[2];
+            Pontok = new byte[8];
+            for (int i = 0; i < Pontok.Length; i++)
+            {
+                Pontok[i] = byte.Parse(t[i + 3]);
+            }
+
+
+
+        }
+
+    }
+}
